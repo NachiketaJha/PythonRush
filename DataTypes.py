1fece6d1c5663ac770d0e84d453693546e1c560b
Nachiketa Jha <nachiketanjha@gmail.com>
2023-11-08 22:52:01 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,21 @@
+#variable is like container
+a=1
+b="Nachiketa"
+print(a)
+print(b)
+#Now a holds 1
+c=68
+print(a+c)
+print("Data Type =", type(a))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+# list = mutable
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+# tuple = immutable
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
+#dictionary = Defined/maped data
---
+++ Day01.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print(5)
+print(17*8)
+print("Hello! \nI'm Nachiketa Jha")
+# Comments ("ctrl+\" """ '''to comment multiple lines)
+# TExt
+# Txt
+# ajkda
+# \n excape sequence character
+print("Hello! \"Im\"\nNachiketa")
+print("Hello", 1, 7, sep="0", end="Keeda\n")
+print("Dam")

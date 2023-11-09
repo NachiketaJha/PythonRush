7f555c30449e13c13d6fa832941367d78f258aad
Nachiketa Jha <nachiketanjha@gmail.com>
2023-11-09 12:33:39 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# a = input("Enter num1:")
+# # a = int
+# # a = input("Enter num2:")
+# # b = int
+a = 9 #str to int conversion
+
+b = 6
+print("Sum is", a, "&", b, "is:", a+b)
+print("Diff is", a, "&", b, "is:", a-b)
+print("Mul is", a, "&", b, "is:", a*b)
+print("Div is", a, "&", b, "is:", a/b)
+# print("FLoor Div is", a, "&", b, "is:", a//b)
+# print("Expo is", a, "&", b, "is:", a**b)
+# print("Mod is", a, "&", b, "is:", a%b)
---
+++ User CalC.py
@@ -0,0 +1,18 @@
+#Basic User Input - String
+print("IDENTITY CHECK-UP")
+Name = input("Who are you?")
+print("Hello!", Name, "\nWelcome back, Hope you find this session interesting :)")
+print("")
+#Calculator
+print("Lets Start Calculating")
+print("")
+a =input("Enter 1st Number:")
+b =input("Enter 2nd Number:")
+
+print("Sum is", a, "&", b, "is:", int(a)+int(b))
+print("Difference is", a, "&", b, "is:", int(a)-int(b))
+print("Multiplication is", a, "&", b, "is:", int(a)*int(b))
+print("Division is", a, "&", b, "is:", int(a)/int(b))
+print("Floor Division is", a, "&", b, "is:", int(a)//int(b))
+print("Modulus is", a, "&", b, "is:", int(a)%int(b))
+print("Expontial is", a, "&", b, "is:", int(a)**int(b))
